954c2d031ff6cad8edbe39933a501858ebc64c70
sridevi <you@example.com>
2021-12-16 20:06:47 +0100

    updated

--- js/form.js
+++ js/form.js
@@ -5,17 +5,88 @@ axios.post('https://reqres.in/api/login',{
     "password":document.getElementById('password').value
 }).then((result)=>{
     html='User Logged In Sucessfully. Token: '+result.data.token
-    document.getElementById('message').innerHTML=html;
-    console.log(result.data);
+  
+    alert('login successful');
 }).catch((error)=>{
-    document.getElementById('message').innerHTML='Login Failed. Reason: '+error.response.data.error
-    alert(error.response.status);
+    
+    alert('invalid login');
 })  
 }
 
-function orDernow()  {
-if (confirm("Plesae fill the form")) {
-  
-document.getElementById("demo").innerHTML = txt;
+const form = document.getElementById('form');
+const Firstname = document.getElementById('Firstname');
+const Lastname = document.getElementById('Lastname');
+const email = document.getElementById('emailAddress');
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    validateInputs();
+});
+const setError = (element, message) => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = message;
+    inputControl.classList.add('error');
+    inputControl.classList.remove('success')
 }
+const setSuccess = element => {
+    console.log(element);
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = '';
+    inputControl.classList.add('success');
+    inputControl.classList.remove('error');
+};
+const isValidEmail = email => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
 }
+
+
+
+const validateInputs = () => {
+    
+    console.log('validateInputs')
+    const FirstnameValue = Firstname.value.trim();
+    const LastnameValue = Lastname.value.trim();
+    const emailValue = email.value.trim();
+    
+
+    if(FirstnameValue === '') {
+        console.log('here')
+        setError(Firstname, 'Firstname is required');
+    } 
+    else {
+        setSuccess(Firstname);
+    }
+
+
+
+
+if(LastnameValue === '') {
+    setError(Lastname, 'Lastname is required');
+} 
+else {
+    setSuccess(Lastname);
+}
+
+
+
+
+
+if(emailValue === '')
+ {
+    setError(email, 'Email is required');
+} 
+else if (!isValidEmail(emailValue))
+ {
+    setError(email, 'Provide a valid email address');
+} 
+else {
+    setSuccess(email);
+}
+};
+
